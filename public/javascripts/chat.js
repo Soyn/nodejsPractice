5836dc11a174f62b9dc17b974abd8dc9b8c94b39
Soyn <w604408482@163.com>
2017-05-09 09:12:36 +0800

    cient-side

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,3 +1,56 @@
 /**
  * Created by AlnordWang on 2017/5/6.
  */
+
+class Chat{
+    constructor(socket){
+        this.socket = socket
+    }
+
+    /**
+     *
+     * @param {String} room
+     * @param {String} text
+     */
+    sendMessage(room, text) {
+        let message = {room: room, text: text}
+        this.socket.emit('message', message)
+    }
+
+    /**
+     *
+     * @param room
+     */
+    changeRoom(room) {
+        this.socket.emit('join', {
+            newroom: room
+        })
+    }
+
+    processCommand(command) {
+        let words = command.split(' ')
+        let command = words[0].substring(1, words[0].length).toLowerCase()
+        let message = false
+
+        switch(command) {
+            case 'join': {
+                words.shift()
+                let room = words.join(' ')
+                this.changeRoom(room)
+                break
+            }
+
+            case 'nick': {
+                words.shift()
+                let name = words.join(' ')
+                this.socket.emit('nameAttempt', name)
+                break
+            }
+
+            default:
+                message = 'Unrecongnize command.'
+                break
+        }
+        return message
+    }
+}
